68492087f9f96a99bda3797228f9b21f8bf88736
Amit Katare <141896620+Amitkatare19@users.noreply.github.com>
2024-11-19 20:00:20 +0530

    12334

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='w-[2000px] h-[500px] p-[20px] bg-[red]'>
+<h1>{import.meta.env.VITE_ADDCHATGPT}</h1>
+      App
+      </div>
+  )
+}
+
+export default App
